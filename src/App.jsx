51ff86035b3e8629cecac0bc1d7f5f5e25531c67
VishalTyagi8974 <115866008+VishalTyagi8974@users.noreply.github.com>
2024-12-26 13:08:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Navbar from '@components/header/navbar';
+import Navbar from '@components/header/Navbar';
 import Footer from '@components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 
