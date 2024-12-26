7173769f0d79d9de6d9dac9a6df82e4378014f18
VishalTyagi8974 <115866008+VishalTyagi8974@users.noreply.github.com>
2024-12-26 13:10:29 +0530

    Update App.jsx footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Navbar from '@components/header/Navbar';
-import Footer from '@components/Footer/Footer';
+import Footer from '@components/footer/Footer';
 import { Outlet } from 'react-router-dom';
 
 function App() {
